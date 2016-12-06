72f83a77782e3a5bf6a687f3a7faf4484c2bbe9b
Kjell Morgenstern <kjell@gmx.de>
2016-12-06 19:18:45 +0100

    Compatible with mina 1.0.3 Bump version

--- lib/mina/scp.rb
+++ lib/mina/scp.rb
@@ -1,18 +1,21 @@
 def scp_transfer(direction, from, to, options = {})
-  remote_prefix = "#{domain!}:"
-  remote_prefix = "#{user}@#{remote_prefix}" if user?
-  command = "scp"
-  command << " -i #{identity_file}" if identity_file?
-  command << " -P #{port}" if port?
-  command << " -r" if options[:recursively]
+  # scp options inspired by Mina::Backend::Remote
+  ensure!(:domain)
+  remote_prefix = "#{fetch(:domain)}:"
+  remote_prefix = "#{fetch(:user)}@#{remote_prefix}" if set?(:user)
+  scp_cmd = "scp"
+  scp_cmd << " -i #{fetch(:identity_file)}" if set?(:identity_file)
+  scp_cmd << " -P #{fetch(:port)}" if set?(:port)
+  scp_cmd << " -r" if options[:recursively]
+  scp_cmd << " -o StrictHostKeyChecking=no"
   case direction
   when :up   then to   = remote_prefix + to
   when :down then from = remote_prefix + from
   end
-  command << " #{from} #{to}"
-  print_command command if options[:verbose]
-  result = %x[#{command}]
-  puts result unless result == ""
+  scp_cmd << " #{from} #{to}"
+  run :local do
+    command scp_cmd
+  end
 end
 
 def scp_download(from, to, options = {})
--- lib/mina/scp/version.rb
+++ lib/mina/scp/version.rb
@@ -1,5 +1,5 @@
 module Mina
   module Scp
-    VERSION = "0.1.2"
+    VERSION = "0.9.0"
   end
 end
